aabcefae5b716d701eb1b25f29ff6413f47e1a75
yangwei <1047628463@qq.com>
2016-08-23 21:30:04 +0800

    Initial Commit

---
+++ cpp_primer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp_primer
+//
+//  Created by yangwei on 16/8/23.
+//  Copyright © 2016年 yangwei. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
